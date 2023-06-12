34bcad4834ad2c8e115c31bf6878602217929113
KugutEugene <72006667+KugutEugene@users.noreply.github.com>
2023-06-12 13:11:36 +0500

    Update WebOSTVService.m
    fix find topViewController

--- Services/WebOSTVService.m
+++ Services/WebOSTVService.m
@@ -364,8 +364,28 @@
     [topViewController presentViewController:alertController animated:YES completion:nil];
 }
 
+-(UIViewController *)rootViewController {
+    if (@available(iOS 13, *)) {
+        NSArray *scenes = [[[UIApplication sharedApplication] connectedScenes] allObjects];
+        for (UIWindowScene *scene in scenes) {
+            NSArray *windows = [scene windows];
+            for (UIWindow  *window in windows) {
+                if (window.isKeyWindow) {
+                    return window.rootViewController;
+                }
+            }
+        }
+
+        return nil;
+    } else {
+        return [UIApplication sharedApplication].keyWindow.rootViewController;
+    }
+
+}
+
 - (UIViewController *)topViewController {
-    UIViewController *topViewController = [UIApplication sharedApplication].keyWindow.rootViewController;
+    UIViewController *topViewController = [self rootViewController];
+
     while (topViewController.presentedViewController) {
         topViewController = topViewController.presentedViewController;
     }
